5c31c4219932f05c95b3f5ad8546a049f0bf7ee1
Yannick Miñano <leoynick@gmail.com>
2019-09-24 23:20:35 +0200

    Add ListOfPhotoCards and PhotoCards components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 
 import { ListOfCategories } from "./components/ListOfCategories";
+import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
 import { GlobalStyle } from "./GlobalStyles";
 
 export const App = () => (
   <Fragment>
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </Fragment>
 );
