9ee6090fb375cd867058aab2f0b8738443eb4543
Yannick Miñano <leoynick@gmail.com>
2019-09-24 23:33:25 +0200

    Add Logo component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 
+import { Logo } from "./components/Logo";
 import { ListOfCategories } from "./components/ListOfCategories";
 import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
 import { GlobalStyle } from "./GlobalStyles";
@@ -7,6 +8,7 @@ import { GlobalStyle } from "./GlobalStyles";
 export const App = () => (
   <Fragment>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </Fragment>
