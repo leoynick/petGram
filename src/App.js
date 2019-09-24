805a23c9a0a0e7e503e9a6cffa5cf574a3f225d6
Yannick Miñano <leoynick@gmail.com>
2019-09-24 16:15:52 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+import { ListOfCategories } from "./components/ListOfCategories";
+import { GlobalStyle } from "./GlobalStyles";
+
+export const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <ListOfCategories />
+  </Fragment>
+);
