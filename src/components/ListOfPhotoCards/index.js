83117391a999ce37415393a5697959ed4cb7b518
Yannick Miñano <leoynick@gmail.com>
2019-09-27 12:06:52 +0200

    Use localStorage for lbutton like Add global hooks

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -7,7 +7,7 @@ export const ListOfPhotoCards = () => {
     <ul>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(id => (
         <li key={id}>
-          <PhotoCard />
+          <PhotoCard id={id} />
         </li>
       ))}
     </ul>
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,5 +1,7 @@
-import React, { useRef, useEffect, useState } from 'react'
-import { MdFavoriteBorder } from 'react-icons/md'
+import React from 'react'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { Article, ImgWrapper, Img, Button } from './styles'
 
@@ -7,26 +9,11 @@ const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1553987882-91d92995e16c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
+  const key = `like-${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
+  const [show, element] = useNearScreen()
 
-  useEffect(() => {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(entries => {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-
-      observer.observe(element.current)
-    })
-  }, [element])
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -39,8 +26,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               </ImgWrapper>
             </a>
 
-            <Button>
-              <MdFavoriteBorder size='32px' />
+            <Button onClick={() => setLiked(!liked)}>
+              <Icon size='32px' />
               {likes} likes!
             </Button>
           </>
