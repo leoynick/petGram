b694912c3479f707662f7a41407e7170fb453aaf
Yannick Miñano <leoynick@gmail.com>
2019-09-30 18:43:59 +0200

    Add navBar active with arial-current

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import { Link as LinkRouter } from '@reach/router'
 
+import { fadeIn } from '../../styles/animation'
+
 export const Link = styled(LinkRouter)`
   align-items:center;
   color: #888;
@@ -9,6 +11,19 @@ export const Link = styled(LinkRouter)`
   justify-content:center;
   text-decoration:none;
   width:100%;
+  
+  &[aria-current] {
+    color:#000;
+
+    &:after {
+      ${fadeIn({ time: '0.5s' })};
+      content: '.';
+      position: absolute;
+      bottom: 0;
+      font-size: 34px;
+      line-height: 30px;
+    }
+  }
 `
 
 export const Nav = styled.nav`
