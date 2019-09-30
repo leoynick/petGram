d4ed0f6374d4dc9ace5f73eaff533c452b230d74
Yannick Miñano <leoynick@gmail.com>
2019-09-30 18:30:39 +0200

    Create NavBar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Router } from '@reach/router'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
 
 import { GlobalStyle } from './styles/GlobalStyles'
 
@@ -17,7 +18,7 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
-
+      <NavBar />
     </>
   )
 }
