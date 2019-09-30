49e9509de68fe261b6601a4517141704a7b9fa7c
Yannick Miñano <leoynick@gmail.com>
2019-09-30 11:31:39 +0200

    Add button like mutation

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
+
 import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 import { Article, ImgWrapper, Img } from './styles'
 
@@ -14,8 +16,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [liked, setLiked] = useLocalStorage(key, false)
   const [show, element] = useNearScreen()
 
-  const handleFavClick = () => setLiked(!liked)
-
   return (
     <Article ref={element}>
       {
@@ -26,7 +26,21 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                 <Img src={src} />
               </ImgWrapper>
             </a>
-            <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+            <ToggleLikeMutation>
+              {
+                toggleLike => {
+                  const handleFavClick = () => {
+                    !liked && toggleLike({
+                      variables: {
+                        input: { id }
+                      }
+                    })
+                    setLiked(!liked)
+                  }
+                  return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+                }
+              }
+            </ToggleLikeMutation>
           </>
       }
     </Article>
