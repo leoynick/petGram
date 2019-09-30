b83fb9a75083242e1a4426d112954c29e32bbbf0
Yannick Miñano <leoynick@gmail.com>
2019-09-30 20:05:59 +0200

    Add Favs, User and NotRegister pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import { Router } from '@reach/router'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisterUser } from './pages/NotRegisterUser'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 
 import { GlobalStyle } from './styles/GlobalStyles'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -18,6 +25,20 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {({ isAuth }) =>
+          isAuth
+            ? (
+              <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>)
+            : (
+              <Router>
+                <NotRegisterUser path='/favs' />
+                <NotRegisterUser path='/user' />
+              </Router>)}
+      </UserLogged>
       <NavBar />
     </>
   )
---
+++ src/pages/NotRegisterUser.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const NotRegisterUser = () => <h1>NotRegsiterUser!</h1>
