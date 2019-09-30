88f62735cfcd00872859b42ba6071703131afffa
Yannick Miñano <leoynick@gmail.com>
2019-09-30 17:33:08 +0200

    Detail with reach/router

--- src/App.js
+++ src/App.js
@@ -2,29 +2,22 @@ import React from 'react'
 import { Router } from '@reach/router'
 
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
 import { GlobalStyle } from './styles/GlobalStyles'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : (
-            <Router>
-              <Home path='/' />
-              <Home path='/pet/:categoryId' />
-            </Router>
-          )
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+      </Router>
+
     </>
   )
 }
