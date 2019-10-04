ee8022c2f16f238b8ef47639f93f68025a7f7556
Yannick Miñano <leoynick@gmail.com>
2019-10-04 12:36:00 +0200

    session persistance and like as user. remove localStore for PhtoCard

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from '@reach/router'
 
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { FavButton } from '../FavButton'
@@ -12,9 +11,7 @@ import { Article, ImgWrapper, Img } from './styles'
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1553987882-91d92995e16c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
 
   return (
@@ -31,12 +28,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               {
                 toggleLike => {
                   const handleFavClick = () => {
-                    !liked && toggleLike({
+                    toggleLike({
                       variables: {
                         input: { id }
                       }
                     })
-                    setLiked(!liked)
                   }
                   return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
                 }
