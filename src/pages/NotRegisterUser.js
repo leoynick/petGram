0045d53d31b5d957e2c290043a11b8331d4378b8
Yannick Miñano <leoynick@gmail.com>
2019-10-04 10:16:41 +0200

    Add login mutation

--- src/pages/NotRegisterUser.js
+++ src/pages/NotRegisterUser.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
 export const NotRegisterUser = () => (
   <Context.Consumer>
@@ -30,7 +31,21 @@ export const NotRegisterUser = () => (
               }
             </RegisterMutation>
 
-            <UserForm onSubmit={activateAuth} title='Iniciar Sesion' />
+            <LoginMutation>
+              {(login, { data, loading, error }) => {
+                const onSubmit = ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  login({ variables })
+                    .then(activateAuth)
+                }
+
+                const errorMsg = error && 'La contraseña es incorrecta o el usuario no existe'
+
+                return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Iniciar Sesion' />
+              }}
+            </LoginMutation>
+
           </>
         )
       }
